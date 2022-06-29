e8544fc25ef37333bbb29bf04d1fc22045aca040
sergioaugustoss <sergioaugusto183@gmail.com>
2022-06-29 18:54:29 -0300

    Adiciona requisito 05

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,45 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+// import fetchApi from './Gravata';
+import md5 from 'crypto-js/md5';
 
-// class Header extends Component {
-//   async componentDidMount() {
-//     const gravataApi = await fetchApi();
-//   }
+class Header extends Component {
+  state = {
+    avatar: '',
+  }
 
-//     render() {
-//       return (
-//         <header>
-//           <span>PLayer: </span>
-//           <h1 data-testid="header-player-name"> </h1>
+  async componentDidMount() {
+    const { email } = this.props;
+    const HASH = md5(email).toString();
+    this.setState({ avatar: HASH });
+  }
 
-//           <span>Score: </span>
-//           <span id="score" data-testid="header-scores">0</span>
-//         </header>
-//       );
-//     }
+  render() {
+    const { avatar } = this.state;
+    return (
+      <header>
+        <span>Player: </span>
+        <img
+          src={ `https://www.gravatar.com/avatar/${avatar}` }
+          alt="profile"
+          data-testid="header-profile-picture"
+        />
+        <h1 data-testid="header-player-name"> </h1>
+        <span>Score: </span>
+        <span id="score" data-testid="header-scores">0</span>
+      </header>
+    );
+  }
+}
 
-//   export default Head
+Header.propTypes = {
+  email: PropTypes.string,
+
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Header);
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,25 +1,29 @@
-/* eslint-disable react/prop-types */
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
-// import { fetchQuestionsThunk } from '../redux/actions';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Header from '../components/Header';
+import { fetchQuestionsThunk } from '../redux/actions';
 
 class Game extends Component {
-//   componentDidMount() {
-//     const { dispatch, token } = this.props;
-//     dispatch(fetchQuestionsThunk(token));
-//   }
+  componentDidMount() {
+    const { dispatch, token } = this.props;
+    dispatch(fetchQuestionsThunk(token));
+  }
 
   render() {
     return (
-      <h1> teste </h1>
+      <Header />
     );
   }
 }
 
-// Login.propTypes = {
-//   dispatch: PropTypes.func,
-//   user: PropTypes.shape({ token: PropTypes.string }),
-// }.isRequired;
+const mapStateToProps = (state) => ({
+  token: state.user.token,
+});
 
-export default Game;
+Game.propTypes = {
+  dispatch: PropTypes.func,
+  user: PropTypes.shape({ token: PropTypes.string }),
+}.isRequired;
+
+export default connect(mapStateToProps)(Game);
